a9733e17194aa71b876c61ad376734166dc7428c
Panayot Kindalov <panayotkindalov@gmail.com>
2016-06-12 21:50:28 +0300

    Write a program to read a matrix of words (space separated) and rotate it on the right

---
+++ Lab/RotateAMatrix/RotateAMatrix.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RotateAMatrix
+{
+    class RotateAMatrix
+    {
+        static void Main(string[] args)
+        {
+            int rows = int.Parse(Console.ReadLine());
+            int cols = int.Parse(Console.ReadLine());
+
+            string[,] matrix = new string[rows, cols];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                //Console.Write($"row {row} -> ");
+                string[] sign = Console.ReadLine().Split(' ').ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = sign[col];
+
+                }
+
+            }
+
+
+            for (int col = 0; col < cols; col++)
+            {
+
+                for (int row = rows - 1; row >= 0; row--)
+                {
+                    Console.Write(matrix[row, col] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
