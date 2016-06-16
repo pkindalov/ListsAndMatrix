61c32a36fb712ef175239260c944ef13c8e52919
Panayot Kindalov <panayotkindalov@gmail.com>
2016-06-16 10:36:23 +0300

    Write a program that reads sequence of numbers and special bomb number with a certain power. Your task is to detonate every occurrence of the special bomb number and according to its power his neighbors from left and right. Detonations are performed from left to right and all detonated numbers disappear. Finally print the sum of the remaining elements in the sequence.

---
+++ Exercises/BombNumbers/BombNumbers.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+
+namespace BombNumbers
+{
+    class BombNumbers
+    {
+        static void Main()
+        {
+            int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int[] input = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            int bombNumber = input[0];
+            int bombPower = input[1];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] == bombNumber)
+                {
+                    int leftEnd = Math.Max(0, i - bombPower);
+                    int rightEnd = Math.Min(numbers.Length - 1, i + bombPower);
+
+                    for (int j = leftEnd; j <= rightEnd; j++)
+                    {
+                        numbers[j] = 0;
+                    }
+                }
+            }
+            Console.WriteLine(numbers.Sum());
+        }
+    }
+}
