a64e2b6bb7ba8215523633696ea3f98e82f6698e
Panayot Kindalov <panayotkindalov@gmail.com>
2016-06-16 10:41:10 +0300

    You are given a square matrix representing distances between cities. City names are numbers from 0 to N. Also you are a given a sequence of cities that needs to be visited in that order. You start from city 0. Your task is to write a program that reads the distances matrix, the sequence of numbers representing the numbers of the cities that will be visited starting from city 0 and finally print the total distance of the route.

---
+++ Exercises/Tour/Tour.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tour
+{
+    class Tour
+    {
+        static void Main()
+        {
+            int size = int.Parse(Console.ReadLine());
+            var matrix = GetMatrix(size);
+            var distance = GetTourDistance(matrix);
+
+            Console.WriteLine(distance);
+        }
+
+        static int GetTourDistance(int[,] matrix)
+        {
+            int[] path =
+                Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int distance = 0;
+            int currentCity = 0;
+            foreach (int city in path)
+            {
+                distance += matrix[currentCity, city];
+                currentCity = city;
+            }
+            return distance;
+        }
+
+        static int[,] GetMatrix(int size)
+        {
+            int[,] matrix = new int[size, size];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] currentRow =
+                    Console.ReadLine()
+                        .Split(new[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries)
+                        .Select(int.Parse)
+                        .ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = currentRow[col];
+                }
+            }
+            return matrix;
+        }
+    }
+}
